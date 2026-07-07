a777d48b6e0ddd7ac24bef8ca712ef55ce5dbdf2
YELEMCOURE OUAHABOU <ouahabouyelemcoure@gmail.com>
2026-07-07 09:50:48 +0000

    Mise à jour

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -89,7 +89,7 @@ const Navbar = () => {
                         </button>
                     </Link>
                     <Link 
-                        href="https://stock-one-sepia.vercel.app/"
+                        href="https://stock.yosite.fun/"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
