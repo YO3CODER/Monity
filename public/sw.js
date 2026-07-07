5191475b1c8cf0c1258736f602c2d463fc675c5b
YELEMCOURE OUAHABOU <ouahabouyelemcoure@gmail.com>
2026-07-07 09:40:52 +0000

    Mise à jour

---
+++ public/sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = "monity-facture-v1";
+
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
